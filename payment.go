1cce1ad9b1cdd7c5e9b45cc41ecd4563d2923eac
Helmi Ruza <helmiruza@gmail.com>
2019-12-22 20:01:42 +0800

    dep init

--- payment.go
+++ payment.go
@@ -9,24 +9,8 @@ import (
   models "github.com/helmiruza/senangpay-go/models"
 )
 
-func CreatePayment(data models.Payment) (models.PaymentResponse, models.Error) {
+func CreatePaymentLink(data models.Payment) (string) {
   URL += fmt.Sprintf("/payment/%s", MERCHANTID)
 
-  requestBody, _ := json.Marshal(data)
-
-  client := &http.Client{}
-
-  req, _ := http.NewRequest("POST", URL, bytes.NewBuffer(requestBody))
-  req.SetBasicAuth(APIKEY, "")
-  req.Header.Set("Content-type", "application/json")
-
-  resp, _ := client.Do(req)
-  body, err := ioutil.ReadAll(resp.Body)
-
-  obj, err1 := response(body, err, "bill")
-  objString, _ := json.Marshal(obj)
-  k := models.BillResponse{}
-  json.Unmarshal(objString, &k)
-
-  return k, err1
+  return URL
 }
